30857ed6ffb7da8f52072aaf25119d2f9d7bfb5a
acaramida <a.caramida@campus.fct.unl.pt>
2020-03-03 12:07:22 +0000

    sequencial done but with bug on output result

--- src/approxPi/Main.java
+++ src/approxPi/Main.java
@@ -3,32 +3,39 @@ package approxPi;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		InputStreamReader r = new InputStreamReader(System.in); 
-	    BufferedReader br = new BufferedReader(r);
-	    System.out.println("Enter number of simulations");    
-	    String n = br.readLine();    
+	    BufferedReader br = new BufferedReader(r);  
+	    String[] n = br.readLine().split(" ");   
 	    
-		final String name = "approxPi ";
-		final long start = System.currentTimeMillis();
-		int points = Integer.parseInt(n);
-		int p_inside = 0;
-		float pi_esti = 0;
+		final long start = System.nanoTime();
+		int points = Integer.parseInt(n[n.length-1]);
+		double pi_esti = 0;
+		float circle_r = 1;
+		int count_inside = 0;
 		
+		for(int i = 0; i < points;i++)  {
+			double x = ThreadLocalRandom.current().nextDouble();
+			double y = ThreadLocalRandom.current().nextDouble();
+			
+			double cat_x = 1-x;
+			double cat_y = 1-y;
+			if(cat_x*cat_x + cat_y*cat_y <= circle_r*circle_r)
+				count_inside++;
+		}
 		
-		//TODO implementation
-		
-		
-		final long end = System.currentTimeMillis()-start;
-		System.out.println("$ " + name + points);
+		pi_esti = count_inside/points * 4.0;
+		final long end = System.nanoTime()-start;
 		System.out.println("Total number of points: " + points);
-		System.out.println("Points within circle: " + p_inside);
+		System.out.println("Points within circle: " + count_inside);
 		System.out.println("Pi estimation: " + pi_esti);
 		
-		System.out.println("Execution time: " + end);
+		if(n[0].equalsIgnoreCase("time"))
+			System.out.println("Execution time: " + end);
 	}
 
 }
