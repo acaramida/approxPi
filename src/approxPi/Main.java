51afdddbbe34b3d4cb021d01b2948e0528989aaf
acaramida <a.caramida@campus.fct.unl.pt>
2020-03-03 11:04:45 +0000

    number of simulations input

--- src/approxPi/Main.java
+++ src/approxPi/Main.java
@@ -1,13 +1,24 @@
 package approxPi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		InputStreamReader r = new InputStreamReader(System.in); 
+	    BufferedReader br = new BufferedReader(r);
+	    System.out.println("Enter number of simulations");    
+	    String n = br.readLine();    
+	    
 		final String name = "approxPi ";
 		final long start = System.currentTimeMillis();
-		int points = 0;//TODO
+		int points = Integer.parseInt(n);
 		int p_inside = 0;
 		float pi_esti = 0;
+		
+		
 		//TODO implementation
 		
 		
