0924b89e3cc8f29ae9acd08b01cd0ce19e10cbae
acaramida <a.caramida@campus.fct.unl.pt>
2020-03-08 14:19:50 +0000

    replaced buffer reader input with cmd args, missing exec time output

--- src/approxPi/Main.java
+++ src/approxPi/Main.java
@@ -1,19 +1,12 @@
 package approxPi;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		InputStreamReader r = new InputStreamReader(System.in); 
-	    BufferedReader br = new BufferedReader(r);  
-	    String[] n = br.readLine().split(" ");   
-	    
-		final long start = System.nanoTime();
-		double points = Integer.parseInt(n[n.length-1]);
+		double points = Integer.parseInt(args[0]);
 		double pi_esti = 0;
 		double circle_r = 1;
 		double count_inside = 0;
@@ -29,13 +22,10 @@ public class Main {
 		}
 		
 		pi_esti = count_inside/points * 4.0;
-		final long end = System.nanoTime()-start;
 		System.out.println("Total number of points: " + points);
 		System.out.println("Points within circle: " + count_inside);
 		System.out.println("Pi estimation: " + pi_esti);
-		
-		if(n[0].equalsIgnoreCase("time"))
-			System.out.println("Execution time: " + end);
+	
 	}
 
 }
