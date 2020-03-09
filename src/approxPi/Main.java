7aeef7d159402d0ca05e9a7c6dcb2bce112eacb9
acaramida <a.caramida@campus.fct.unl.pt>
2020-03-09 22:15:24 +0000

    parallel version done, still missing exec time.

--- src/approxPi/Main.java
+++ src/approxPi/Main.java
@@ -6,28 +6,75 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		double points = 0;
 		
-		points = Integer.parseInt(args[0]);
+		long start = System.currentTimeMillis();
 		
-		double circle_r = 1;
-		double count_inside = 0;
+		double points = Double.parseDouble(args[0]);
+		int threads = Integer.parseInt(args[1]);
+
+		double count_inside_all = 0;
+		Multithreading[] all_threads = new Multithreading[threads];
 		
-		for(int i = 0; i < points;i++)  {
-			double x = ThreadLocalRandom.current().nextDouble();
-			double y = ThreadLocalRandom.current().nextDouble();
-			
-			double cat_x = 1-x;
-			double cat_y = 1-y;
-			if(cat_x*cat_x + cat_y*cat_y <= circle_r*circle_r)
-				count_inside++;
-		}
+        for (int i=0; i<threads; i++) 
+        { 
+        	all_threads[i] = new Multithreading(points/threads);
+        	all_threads[i].start();
+        } 
 		
-		double pi_esti = count_inside/points * 4.0;
-		System.out.println("Total number of points: " + points);
-		System.out.println("Points within circle: " + count_inside);
+        for (int i=0; i<threads; i++) 
+        {            
+            try {
+				all_threads[i].join();
+				count_inside_all += all_threads[i].count_inside;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        } 
+        
+		double pi_esti = count_inside_all/points * 4.0;
+		
+		long end = System.currentTimeMillis() - start;
+		
+		System.out.println("Total number of points: " + Integer.parseInt(args[0]));
+		System.out.println("Points within circle: " + count_inside_all);
 		System.out.println("Pi estimation: " + pi_esti);
+		System.out.println("Time: " + end + " ms");
 		
 	}
 
 }
+
+class Multithreading extends Thread { 
+	
+	double count_inside;
+	private double points;
+	private double circle_r;
+	
+	public Multithreading(double points) {
+		this.points = points;
+		count_inside = 0;
+		circle_r = 1;
+	}
+	
+    public void run() 
+    { 
+        try
+        { 
+        	for(int i = 0; i < points;i++)  {
+    			double x = ThreadLocalRandom.current().nextDouble();
+    			double y = ThreadLocalRandom.current().nextDouble();
+    			
+    			double cat_x = 1-x;
+    			double cat_y = 1-y;
+    			if(cat_x*cat_x + cat_y*cat_y <= circle_r*circle_r)
+    				count_inside++;
+    		}
+  
+        } 
+        catch (Exception e) 
+        { 
+            // Throwing an exception 
+            System.out.println ("Exception is caught"); 
+        } 
+    } 
+} 
