2d4dbcd1f5d7e0ee9256061e164198627674590a
Alexandru Caramida <a.caramida@campus.fct.unl.pt>
2021-03-29 21:11:29 +0100

    Updated bash script, added -b flag for tests

--- Main.java
+++ Main.java
@@ -7,66 +7,68 @@ public class Main {
 		long start = System.nanoTime();
 		double points = Double.parseDouble(args[0]);
 		int threads = Integer.parseInt(args[1]);
+		int flag = 0;
+		if (args[args.length - 1].equalsIgnoreCase("-b")) {
+			flag = 1;
+		}
 
 		double count_inside_all = 0;
 		Multithreading[] all_threads = new Multithreading[threads];
-		
-        for (int i=0; i<threads; i++) 
-        { 
-        	all_threads[i] = new Multithreading(points/threads);
-        	all_threads[i].start();
-        } 
-		
-        for (int i=0; i<threads; i++) 
-        {            
-            try {
+
+		for (int i = 0; i < threads; i++) {
+			all_threads[i] = new Multithreading(points / threads);
+			all_threads[i].start();
+		}
+
+		for (int i = 0; i < threads; i++) {
+			try {
 				all_threads[i].join();
 				count_inside_all += all_threads[i].count_inside;
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-        } 
-        		
-		double pi_esti = count_inside_all/points * 4.0;
-		
-		double end = (double)(System.nanoTime()- start) * 1e-9;
+		}
 
-		System.out.println("Total number of points: " + points);
-		System.out.println("Points within circle: " + count_inside_all);
-		System.out.println("Pi estimation: " + pi_esti);
-		System.out.printf("Execution time (s): %.3f \n", end);
-		
+		double pi_esti = count_inside_all / points * 4.0;
+
+		double end = (double) (System.nanoTime() - start) * 1e-9;
+
+		if (flag == 1) {
+			System.out.printf("%.0f\t%d\t%.3f\n", points, threads, end);
+		} else {
+			System.out.printf("Total number of points: %.1g\n", points);
+			System.out.println("Points within circle: " + count_inside_all);
+			System.out.println("Pi estimation: " + pi_esti);
+			System.out.printf("Execution time (s): %.3f \n", end);
+		}
+
 	}
 
 }
 
-class Multithreading extends Thread { 
-	
+class Multithreading extends Thread {
+
 	double count_inside;
 	private double points;
-	
+
 	public Multithreading(double points) {
 		this.points = points;
 		count_inside = 0;
 	}
-	
-    public void run() 
-    { 
-        try
-        { 
-        	for(int i = 0; i < points;i++)  {
-    			double x = ThreadLocalRandom.current().nextDouble();
-    			double y = ThreadLocalRandom.current().nextDouble();
-    			
-    			if(x * x + y * y <= 1)
-    				count_inside++;
-    		}
-  
-        } 
-        catch (Exception e) 
-        { 
-            // Throwing an exception 
-            System.out.println ("Exception is caught"); 
-        } 
-    } 
-} 
+
+	public void run() {
+		try {
+			for (int i = 0; i < points; i++) {
+				double x = ThreadLocalRandom.current().nextDouble();
+				double y = ThreadLocalRandom.current().nextDouble();
+
+				if (x * x + y * y <= 1)
+					count_inside++;
+			}
+
+		} catch (Exception e) {
+			// Throwing an exception
+			System.out.println("Exception is caught");
+		}
+	}
+}
